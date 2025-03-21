1fe6e08518af6fb269e733314d55e6d9717f1f3a
Inchino <alessandro.incalza22@gmail.com>
2025-03-21 10:48:10 +0100

    Inizio

---
+++ GestionaleHotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionaleHotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
